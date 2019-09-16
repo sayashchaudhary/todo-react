76b1e1ed825cf6f94d3f412ee678e5a10986e176
Sayash Chaudhary <sayashchaudhary@gmail.com>
2019-09-16 18:51:11 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import TodoList from './TodoList';
+import TodoItems from "./TodoItems";
+
+class App extends Component {
+
+    inputElement = React.createRef();
+
+    constructor() {
+        super();
+        this.state = {
+            items: [],
+            currentItem: {text: '', key: ''}
+        }
+    }
+
+    handleInputHandler = e => {
+        const itemText = e.target.value;
+        const currentItem = {text: itemText, key: Date.now()};
+        this.setState({currentItem});
+        console.log('Hello World !');
+    };
+
+    addItemHandler = e => {
+        e.preventDefault();
+        const newItem = this.state.currentItem;
+        if (newItem.text !== '') {
+            console.log('new item');
+            const items = [...this.state.items, newItem];
+            this.setState({
+                items: items,
+                currentItem: {text: '', key: ''}
+            })
+        }
+        console.log('Add item to the list')
+    };
+
+    deleteItemHandler = key => {
+        const filteredItems = this.state.items.filter(item => {
+            return item.key !== key
+        });
+        this.setState({
+            items: filteredItems
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <TodoList
+                    addItem={this.addItemHandler}
+                    inputElement={this.inputElement}
+                    handleInput={this.handleInputHandler}
+                    currentItem={this.state.currentItem}
+                />
+                <TodoItems entries={this.state.items} deleteItem={this.deleteItemHandler}/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class TodoList extends Component {
+
+    componentDidUpdate() {
+        this.props.inputElement.current.focus();
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <div className="header">
+                    <form onSubmit={this.props.addItem}>
+                        <input
+                            placeholder="Todo"
+                            ref={this.props.inputElement}
+                            value={this.props.currentItem.text}
+                            onChange={this.props.handleInput}
+                        />
+                        <button type="submit">Add Todo</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
